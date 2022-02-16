02437f7cda501f4cd87770859dc45a3bdbb378f6
Leloup <alexleloup@yahoo.fr>
2022-02-16 14:53:59 +0100

    Add Javadoc and executable

--- Readme.md
+++ Readme.md
@@ -10,3 +10,5 @@ Code
 Executable
 
 JavaDoc
+
+
---
+++ documentation/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"destination"},{"l":"middle"},{"l":"origin"},{"l":"strategy"}]
